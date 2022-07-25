d36fb664af102c69f2af2266e10ef89e330dcdc9
Zuckuu <entbit12@gmail.com>
2022-07-25 02:04:58 -0500

    updating my portfolio

---
+++ pages/works/e-library.js
@@ -0,0 +1,52 @@
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => (
+  <Layout title="E-library">
+    <Container>
+      <Title>
+        E-library <Badge>2022-</Badge>
+      </Title>
+      <P>
+      This is e-commerce website for books. It got features books, sorting algorithm, and a working cart. (Not Real)
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href="https://zuckuu.github.io/e-library-react/" target="_blank">
+          https://zuckuu.github.io/e-library-react/ <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Windows/macOS/Linux/iOS/Android</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>Html, Css, Java, React</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Blogpost</Meta>
+          <span>One of my first projects!</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/works/library.png" alt="E-library" />
+      <WorkImage src="/images/works/library-01.png" alt="E-library" />
+      <WorkImage src="/images/works/library-02.png" alt="E-library" />
+      
+    </Container>
+  </Layout>
+)
+
+export default Work
+export { getServerSideProps } from '../../components/chakra'
---
+++ pages/works/google-clone.js
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  UnorderedList,
+  Heading,
+  Center
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => (
+  <Layout title="Google-Clone">
+    <Container>
+      <Title>
+        Google-Clone <Badge>2022</Badge>
+      </Title>
+      <P>This is a google-clone that feature a working search engine.</P>
+      <List ml={4} my={4}>
+      <ListItem>
+          <Meta>Website</Meta>
+          <Link href="https://zuckuu.github.io/google-clone/" target="_blank">
+          https://zuckuu.github.io/google-clone/ <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span> HTML, CSS, JavaScript</span>
+        </ListItem>
+        <ListItem>
+        <Meta>Platform</Meta>
+        <span>Windows/macOS/Linux/iOS/Android</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Blogpost</Meta>
+          <span>A pratice on how to improve my skill on css and html.</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/works/google-clone.png" alt="google-clone" />
+    </Container>
+  </Layout>
+)
+
+export default Work
+export { getServerSideProps } from '../../components/chakra'
